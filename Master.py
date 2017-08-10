d0db3e56140d8bcd0841e2eed3e22a458684e8b1
Arrieta Marina <arrietamarina12@gmail.com>
2017-08-09 21:11:10 -0300

    mio marina

--- Master.py
+++ Master.py
@@ -1,3 +1,18 @@
 lista_palabras = "informatorio"
 
 print (lista_palabras)
+
+# Historia 4: quiero que al acertar la letra me muestre su posición/es en caso de repetirse
+# para: ver como se va formando la palabra.
+
+# Tarea 1: Haga una Funcion de seleccion, en caso de ser TRUE, en un bucle localice la posicion de la letra 
+#acertada y reemplace en la cadena auxiliar de "guiones" por la letra en dicha posicion. Retorne la cadena 
+#auxiliar.
+
+# Tarea 2: Hacer una función que muestre la cadena auxiliar.
+
+def seleccion(a,b):
+
+	
+
+	return resultado
