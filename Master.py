7da5c7ff4544d0ba86ef8e8ee734187861c772f6
gatomono <facu_nahu_48@hotmail.com>
2017-08-11 19:18:16 -0300

    Claudio mio

--- Master.py
+++ Master.py
@@ -1,3 +1,15 @@
 lista_palabras = "informatorio"
+import random
+num = random.randint(0,10)
 
-print (lista_palabras)
+def Crearlista():
+	lista = ["Mauricio","Alan","Marina","Facundo"]
+	devuelve = random.choice(lista)
+	return devuelve
+
+def Calculalongitud():
+	longitud = len(lista[num])
+	print "El numero de letras en la palabra es: "
+	return longitud
+
+
