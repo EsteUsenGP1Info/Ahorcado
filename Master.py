ba74be51183434eb0fa8dc2cfd70eadb5d488010
alan87 <alanpanzich@hotmail.com>
2017-08-11 17:14:44 -0300

    Alan mio menu

--- Master.py
+++ Master.py
@@ -1,3 +1,24 @@
 lista_palabras = "informatorio"
 
 print (lista_palabras)
+
+
+def menu_juego():
+	s = False
+	while s==False:
+		
+		print ("Opciones: ")
+		print ("1-Iniciar Partida\n2 - Ranking\n3 - Salir del juego")
+
+		ing = int(input ("ingrese una opcion: "))
+		if ing ==3:
+			s= True
+			print ("Has salido del juego")
+
+		
+		if ing==1:
+			
+			print ("\nIniciando...\n\n")
+
+			
+			input()
